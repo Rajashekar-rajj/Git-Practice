c3f5c91c43f3a04e4d52f78bd4b4e57f9491915b
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-22 00:39:47 +0530

    installation

--- 07_installation.sh
+++ 07_installation.sh
@@ -2,9 +2,9 @@
 USERID=$(id -u)
 #root user id will be 0
 echo "user id is $USERID"
-if [ $USERID -et 0 ]
+if [ $USERID -ne 0 ]
    then 
-       echo " "Please run this script with root priveleges"
+       echo "Please run this script with root priveleges"
 
        exit 1
 fi
