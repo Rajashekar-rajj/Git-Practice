18533c90f2fc458d9321cb518089ecbe566b5692
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-21 22:55:32 +0530

    variable

---
+++ 03_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter the name"
+read -s USERNAME
+echo "user name is : $USERNAME"
+echo "Please enter the password"
+read -s PASSWORD
+
+echo "your password is $PASSWORD"
