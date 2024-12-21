a1f8a89992e2cf4cfcea7b74b13519525ffc6d8b
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-21 23:47:32 +0530

    conditions

--- 06_condition.sh
+++ 06_condition.sh
@@ -7,7 +7,7 @@ echo "entered number is $NUMBER"
 
 if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "Given number: $NUMBER is greater than 20"
+    echo "Given number is greater than 20"
 else
-    echo "Given number: $NUMBER is less than 20"
+    echo "Given number is less than 20"
 fi
