4debd61f93957deecc88e69153a5f5746eeed9b3
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-25 22:36:55 +0530

    Functions

--- 11_redirectors.sh
+++ 11_redirectors.sh
@@ -30,7 +30,7 @@ VALIDATE(){
 
 }
 USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
+    echo -e "$R USAGE:: $N sudo sh 11_redirector.sh package1 package2 ..."
     exit 1
 }
 
