b140ff7183a3fc47aedd9caba9c2a6cc0af5e67a
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-25 15:22:20 +0530

    Functions

--- 08_functions.sh
+++ 08_functions.sh
@@ -11,16 +11,6 @@ VALIDATE(){
 
 }
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then
-#         echo "$2 is...FAILED"
-#         exit 1
-#     else
-#         echo "$2 is...SUCCESS"
-#     fi
-# }
-
 USERID=$(id -u)
 #root user id will be 0
 echo "user id is $USERID"
---
+++ 09_colours.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+#root user id will be 0
+echo "user id is $USERID"
+if [ $USERID -ne 0 ]
+   then 
+       echo -e "$R Please run this script with root priveleges $N"
+
+       exit 1
+fi
+
+VALIDATE(){
+    if [$1 -eq 0]
+       then
+             echo -e "$G $2 success.... $N"
+             exit 1
+    else
+        echo "$R $2 not success... $G"    
+    fi
+
+}
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed, going to install it..."
+            dnf install nginx -y
+       VALIDATE $? "nginx installed"
+else
+   echo "$G Nginx is already installed, nothing to do.. $N"   
+fi
