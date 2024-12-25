1e3f56ef0f68eeea664065ed6899438b35b6c462
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-25 23:22:30 +0530

    Functions

--- 11_redirectors.sh
+++ 11_redirectors.sh
@@ -1,38 +1,41 @@
-#!/bin/bash
-LOGS_FOLDER="/var/log/shall-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #echo "11_redirector.sh | cut -d "." -f1" --> $0 ->11_redirectors(name modified)
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
-
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-#root user id will be 0
-echo "user id is $USERID"
-if [ $USERID -ne 0 ]
-   then 
-       echo -e "$R Please run this script with root priveleges $N" -a $LOG_FILE
+Y="\e[33m"
 
-       exit 1
-fi
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
 VALIDATE(){
-    if [$1 -eq 0]
-       then
-             echo -e "$G $2 success.... $N" | tee -a $LOG_FILE
-             exit 1
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-        echo "$R $2 not success... $G" | tee -a $LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
-
 }
+
 USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 11_redirector.sh package1 package2 ..."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
 }
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
 
 if [ $# -eq 0 ]
 then
