fe840c1e220461f15650b6589acd68856f7132ae
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-25 15:06:10 +0530

    Functions

--- 08_functions.sh
+++ 08_functions.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo "$2 is...FAILED"
-        exit 1
+    if [$1 -eq 0]
+       then
+             echo "$2 success...."
+             exit 1
     else
-        echo "$2 is...SUCCESS"
+        echo "$2 not success..."    
     fi
+
 }
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo "$2 is...FAILED"
+#         exit 1
+#     else
+#         echo "$2 is...SUCCESS"
+#     fi
+# }
 
 USERID=$(id -u)
 #root user id will be 0
