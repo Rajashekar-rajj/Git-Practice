56b5a3038ba30bd4196cf51af0987ba437ecf569
rajashekar-rajj <rajashekarmekala2468@gmail.com>
2024-12-29 19:39:28 +0530

    auto exit command

--- 12_set.sh
+++ 12_set.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Hello world"
+set -e # setting the automatic exit, if we get error, set -ex for debug
 
-Echoo  "Hello world"
-set -e 
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
+
+echo "Hello World success"
+echooooo "Hello Wolrd failure"
+echo "Hello World after failure"
